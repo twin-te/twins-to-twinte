0a2b780e2edf853e763e22f49c86f599702b7ba1
SIY1121 <sota.0512.0213@gmail.com>
2019-12-20 18:02:02 +0900

    add: README add:  login check

--- pc.js
+++ pc.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Twin:te team.
+ * This source code is licensed under the GPL v3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
+ * Written by SIY1121
+ */
+
 /* 
   urlが
   https://twins.tsukuba.ac.jp/campusweb/campusportal.do?page=main&tabId=rs
--- sp.js
+++ sp.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Twin:te team.
+ * This source code is licensed under the GPL v3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
+ * Written by SIY1121
+ */
+
 if (
   document
     .querySelector("#main-portlet-title")
@@ -7,24 +15,40 @@ if (
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
   const b = document.createElement("button");
   b.append(document.createTextNode("Twin:teにインポート"));
-  b.onclick = () => {
+  b.onclick = async () => {
     const now = document.querySelector("[name=TimeoutForm]+table td").innerText;
     if (window.confirm(`${now}の時間割をインポートします`)) {
+      if (!(await checkTwinteLogin())) {
+        alert("エラー\nアプリ側でTwin:teにログインしてからご利用いただけます");
+        return;
+      }
+
       const lectures = Array.from(
         document.querySelectorAll(".rishu-koma td td")
       )
         .map(el => el.innerText.split("\n")[0])
         .filter((el, i, self) => el !== "\u00A0" && self.indexOf(el) === i);
-      postToTwinte(lectures,document.querySelector('table td').textContent.match(/(\d{4})年度/)[1]);
+
+      postToTwinte(
+        lectures,
+        document.querySelector("table td").textContent.match(/(\d{4})年度/)[1]
+      );
     }
   };
   insertAfter(b, document.querySelector("#main-portlet-title"));
 }
 
+const checkTwinteLogin = async () => {
+  const { ok } = await fetch("https://dev.api.twinte.net/v1/users/me", {
+    credentials: "include"
+  });
+  return ok;
+};
+
 const postToTwinte = async (lectures, year) => {
-  await Promise.all(
+  const res = await Promise.all(
     lectures.map(async l => {
-      await fetch("https://dev.api.twinte.net/v1/timetables/", {
+      const { ok } = await fetch("https://dev.api.twinte.net/v1/timetables/", {
         method: "POST",
         body: JSON.stringify({ lectureCode: l, year }),
         headers: {
@@ -32,7 +56,12 @@ const postToTwinte = async (lectures, year) => {
         },
         credentials: "include"
       });
+      return ok;
     })
   );
-  alert("Twin:teへのインポートが完了しました");
+  alert(
+    `Twin:teへのインポートが完了しました\n新規追加：${
+      res.filter(el => el).length
+    }件`
+  );
 };
