00c5edff4dcdd7a2b29754fec479d4becc7fd2aa
SIY1121 <sota.0512.0213@gmail.com>
2020-03-13 17:01:57 +0900

    update: api url

--- sp.js
+++ sp.js
@@ -39,7 +39,7 @@ if (
 }
 
 const checkTwinteLogin = async () => {
-  const { ok } = await fetch("https://dev.api.twinte.net/v1/users/me", {
+  const { ok } = await fetch("https://api.twinte.net/v1/users/me", {
     credentials: "include"
   });
   return ok;
@@ -48,7 +48,7 @@ const checkTwinteLogin = async () => {
 const postToTwinte = async (lectures, year) => {
   const res = await Promise.all(
     lectures.map(async l => {
-      const { ok } = await fetch("https://dev.api.twinte.net/v1/timetables/", {
+      const { ok } = await fetch("https://api.twinte.net/v1/timetables/", {
         method: "POST",
         body: JSON.stringify({ lectureCode: l, year }),
         headers: {
